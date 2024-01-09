f8bbeec55b3a45039af28f3543b10142316cf2d8
Sulfur-CK <sulfur@chainkemists.com>
2024-01-08 20:11:01 -0800

    feat: added log files for GameplayDebugger

---
+++ Source/CkGameplayDebugger/CkGameplayDebugger_Log.cpp
@@ -0,0 +1,12 @@
+#include "CkGameplayDebugger_Log.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DEFINE_LOG_CATEGORY(CkGameplayDebugger);
+
+namespace ck::gameplay_debugger
+{
+	CK_REGISTER_LOG_FUNCTIONS(CkGameplayDebugger);
+}
+
+// --------------------------------------------------------------------------------------------------------------------

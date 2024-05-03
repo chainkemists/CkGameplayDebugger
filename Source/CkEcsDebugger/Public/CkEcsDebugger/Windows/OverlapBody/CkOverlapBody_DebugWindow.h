d5c0d870edf5e7b3f71cc80a499d7ae06b131a7c
Sulfur-CK <sulfur@chainkemists.com>
2024-05-02 21:33:54 -0700

    feat: added Sensor/Marker overlap debug window, parity with the existing gameplay debugger

---
+++ Source/CkEcsDebugger/Public/CkEcsDebugger/Windows/OverlapBody/CkOverlapBody_DebugWindow.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "CkOverlapBody_DebugConfig.h"
+
+#include "CkEcsDebugger/Windows/CkEcs_DebugWindow.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+struct FCk_Handle_Marker;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+template <typename T_HandleType, typename T_UtilsType>
+class FCk_OverlapBody_DebugWindow : public FCk_Ecs_DebugWindow
+{
+    using Super = FCk_Ecs_DebugWindow;
+
+public:
+    auto
+    Initialize() -> void override;
+
+protected:
+    auto
+    RenderHelp() -> void override;
+
+    auto
+    RenderContent() -> void override;
+
+    auto
+    RenderMenu() -> void;
+
+    virtual auto
+    Get_Title() -> FName = 0;
+
+    virtual auto
+    Get_NumOverlaps(
+        const T_HandleType&) -> FName
+    { return TEXT("X"); };
+
+    virtual auto
+    Get_NumWorldOverlaps(
+        const T_HandleType&) -> FName
+    { return TEXT("X"); };
+
+private:
+    auto
+    RenderTable(
+        const FCk_Handle& InMarkerOwner) -> void;
+
+private:
+    ImGuiTextFilter _Filter;
+    TObjectPtr<UCk_OverlapBody_DebugWindowConfig> _Config;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+class FCk_Marker_DebugWindow : public FCk_OverlapBody_DebugWindow<FCk_Handle_Marker, UCk_Utils_Marker_UE>
+{
+protected:
+    virtual auto
+    Get_Title() -> FName override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+class FCk_Sensor_DebugWindow : public FCk_OverlapBody_DebugWindow<FCk_Handle_Sensor, UCk_Utils_Sensor_UE>
+{
+protected:
+    virtual auto
+    Get_Title() -> FName override;
+
+    virtual auto
+    Get_NumOverlaps(
+        const FCk_Handle_Sensor& InSensor) -> FName override;
+
+    virtual auto
+    Get_NumWorldOverlaps(
+        const FCk_Handle_Sensor& InSensor) -> FName override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+

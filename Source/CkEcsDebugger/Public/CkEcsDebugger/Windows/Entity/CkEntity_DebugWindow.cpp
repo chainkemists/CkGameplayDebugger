ca4dd4e0df26196ba4a8ccea60357c3e86f0e4bf
Nitrogen-CK <nitrogen@chainkemists.com>
2025-04-03 20:20:22 -0400

    feat: Entity DebugWindow now displays the Actor associated with the Entity (if any)

--- Source/CkEcsDebugger/Public/CkEcsDebugger/Windows/Entity/CkEntity_DebugWindow.cpp
+++ Source/CkEcsDebugger/Public/CkEcsDebugger/Windows/Entity/CkEntity_DebugWindow.cpp
@@ -39,13 +39,23 @@ auto
         ImGui::Text(ck::Format_ANSI(TEXT("NetMode: {}"), UCk_Utils_Net_UE::Get_EntityNetMode(SelectionEntity)).c_str());
         ImGui::Text(ck::Format_ANSI(TEXT("NetRole: {}"), UCk_Utils_Net_UE::Get_EntityNetRole(SelectionEntity)).c_str());
 
-        if (const auto TeamEntity = UCk_Utils_Team_UE::Cast(SelectionEntity); ck::IsValid(TeamEntity))
+        if (const auto TeamEntity = UCk_Utils_Team_UE::Cast(SelectionEntity);
+            ck::IsValid(TeamEntity))
         {
             ImGui::Text(ck::Format_ANSI(TEXT("Team: {} (Starts from ZERO)"), UCk_Utils_Team_UE::Get_ID(TeamEntity)).c_str());
         }
         else
         {
             ImGui::Text(ck::Format_ANSI(TEXT("Team: Unknown")).c_str());
+        }
+
+        if (UCk_Utils_OwningActor_UE::Has(SelectionEntity))
+        {
+            ImGui::Text(ck::Format_ANSI(TEXT("Actor: {}"), UCk_Utils_OwningActor_UE::Get_EntityOwningActor(SelectionEntity)).c_str());
+        }
+        else
+        {
+            ImGui::Text(ck::Format_ANSI(TEXT("Actor: None")).c_str());
         }
     }
     else

61d3f33f521641c985e765b13a9207f8bda9ac12
AlexObatake-CK <phosphorus@chainkemists.com>
2025-05-28 11:34:29 -0700

    fix: Move entity selection config to separate file

--- Source/CkEcsDebugger/Public/CkEcsDebugger/Windows/EntitySelection/CkEntitySelection_DebugWindow.h
+++ Source/CkEcsDebugger/Public/CkEcsDebugger/Windows/EntitySelection/CkEntitySelection_DebugWindow.h
@@ -3,71 +3,14 @@
 #include "CogWindowConfig.h"
 
 #include "CkEcsDebugger/Windows/CkEcs_DebugWindow.h"
-
-#include "CkEntitySelection_DebugWindow.generated.h"
+#include "CkEntitySelection_DebugConfig.h"
 
 // --------------------------------------------------------------------------------------------------------------------
 
-class UCk_DebugWindowConfig_EntitySelection;struct ImGuiTextFilter;
-
+struct ImGuiTextFilter;
 using FCogWindowEntityContextMenuFunction = TFunction<void(const FCk_Handle& InEntity)>;
 
 //--------------------------------------------------------------------------------------------------------------------------
-
-UENUM()
-enum class ECkDebugger_EntitiesListDisplayPolicy : uint8
-{
-    OnlyRootEntities,
-    EntityList,
-    EntityHierarchy,
-};
-
-CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECkDebugger_EntitiesListDisplayPolicy);
-
-//--------------------------------------------------------------------------------------------------------------------------
-
-UENUM()
-enum class ECkDebugger_EntitiesListSortingPolicy : uint8
-{
-    ID,
-    EnitityNumber,
-    Alphabetical
-};
-
-CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECkDebugger_EntitiesListSortingPolicy);
-
-//--------------------------------------------------------------------------------------------------------------------------
-
-UENUM()
-enum class ECkDebugger_EntitiesListUpdatePolicy : uint8
-{
-    PerFrame,
-    OnButton,
-    PerSecond,
-    PerTenSeconds
-};
-
-CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECkDebugger_EntitiesListUpdatePolicy);
-
-//--------------------------------------------------------------------------------------------------------------------------
-
-UENUM(Flags)
-enum class ECkDebugger_EntitiesListFragmentFilteringTypes : uint32
-{
-    None,
-    Attribute = 1<<0,
-    Ability = 1<<1,
-    AbilityOwner = 1<<2,
-    Replicated = 1<<3,
-    EntityScript = 1<<4,
-    EntityCollection = 1<<5,
-    Timer = 1<<6,
-};
-
-ENUM_CLASS_FLAGS(ECkDebugger_EntitiesListFragmentFilteringTypes)
-CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECkDebugger_EntitiesListFragmentFilteringTypes);
-
-// --------------------------------------------------------------------------------------------------------------------
 
 class FCk_EntitySelection_DebugWindow : public FCk_Ecs_DebugWindow
 {
@@ -111,33 +54,3 @@ private:
 };
 
 // --------------------------------------------------------------------------------------------------------------------
-
-UCLASS(Config = Cog)
-class UCk_DebugWindowConfig_EntitySelection : public UCogWindowConfig
-{
-    GENERATED_BODY()
-
-public:
-    UPROPERTY(Config)
-    ECkDebugger_EntitiesListDisplayPolicy EntitiesListDisplayPolicy = ECkDebugger_EntitiesListDisplayPolicy::EntityHierarchy;
-
-    UPROPERTY(Config)
-    ECkDebugger_EntitiesListSortingPolicy EntitiesListSortingPolicy = ECkDebugger_EntitiesListSortingPolicy::Alphabetical;
-
-    UPROPERTY(Config)
-    ECkDebugger_EntitiesListUpdatePolicy EntitiesListUpdatePolicy = ECkDebugger_EntitiesListUpdatePolicy::PerFrame;
-
-    UPROPERTY(Config)
-    ECkDebugger_EntitiesListFragmentFilteringTypes EntitiesListFragmentFilteringTypes = ECkDebugger_EntitiesListFragmentFilteringTypes::None;
-
-    virtual void Reset() override
-    {
-        Super::Reset();
-
-        EntitiesListDisplayPolicy = ECkDebugger_EntitiesListDisplayPolicy::EntityHierarchy;
-        EntitiesListSortingPolicy = ECkDebugger_EntitiesListSortingPolicy::Alphabetical;
-        EntitiesListUpdatePolicy = ECkDebugger_EntitiesListUpdatePolicy::PerFrame;
-        EntitiesListFragmentFilteringTypes = ECkDebugger_EntitiesListFragmentFilteringTypes::None;
-    }
-};
-//---------------------------------------------------------------------------------------------------------------------
